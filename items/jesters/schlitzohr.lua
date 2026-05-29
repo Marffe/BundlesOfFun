47545f1e4cca68f925f2ae3362309fef9644f7b0
kierkat10 <kiernan.simpson@gmail.com>
2026-05-28 23:28:28 -0700

    fix schlitzohr

--- items/jesters/schlitzohr.lua
+++ items/jesters/schlitzohr.lua
@@ -4,15 +4,26 @@ SMODS.Joker({
 	pos = { x = 4, y = 2 },
 	cost = 8,
 	rarity = 3,
-	blueprint_compat = true,
+	blueprint_compat = false,
 	atlas = "joker",
 	calculate = function(self, card, context)
 		if context.destroy_card and (context.cardarea == G.play or context.cardarea == "unscored") and G.GAME.current_round.hands_left > 0 then
-            if context.destroy_card == pseudorandom_element(G.play.cards, pseudoseed("j_bof_j_schlitzohr")) then
-                return {
-                    remove = true
-                }
-            end
-        end
-	end,
+			if not G.GAME.bof_schlitzohr_destroyed then
+				if not G.GAME.bof_schlitzohr_target then
+					G.GAME.bof_schlitzohr_target = pseudorandom_element(G.play.cards, pseudoseed("j_bof_j_schlitzohr"))
+				end
+				if context.destroy_card == G.GAME.bof_schlitzohr_target then
+					G.GAME.bof_schlitzohr_destroyed = true
+					G.GAME.bof_schlitzohr_target = nil
+					return {
+						remove = true
+					}
+				end
+			end
+		end
+		if context.after then
+			G.GAME.bof_schlitzohr_target = nil
+			G.GAME.bof_schlitzohr_destroyed = nil
+		end
+	end
 })
